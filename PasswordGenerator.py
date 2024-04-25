f906c4b03d756d382ef96c99093727cbed9c9c96
soniyapaudel <sunnapaudel@gmail.com>
2024-04-25 21:30:26 +0545

    Initial commit

---
+++ PasswordGenerator.py
@@ -0,0 +1,15 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length, nums, special_chars, uppercase, lowercase):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+   
